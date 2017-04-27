ccd4d563201659940c36d7f68b98d9a7a0ed8ac2
maedatoma <maeda_toma@yse-c.net>
2017-04-27 23:06:28 +0900

    fixed Java-file and README.md

--- Timer_toma.java
+++ Timer_toma.java
@@ -13,16 +13,21 @@ import javax.swing.*;
 public class Timer_toma extends JFrame implements ActionListener{
 	// field
 	Timer timer;
-	JPanel panel_Main;		// メイン panel
-	JPanel panel_Label;		// ラベル panel
-	JPanel panel_Button;	// ボタン panel
+	JPanel panel_Title;			// タイトル panel
+	JPanel panel_TimeLabel;		// 時間表示 panel
+	JPanel panel_Button;		// ボタン panel
+	JPanel panel_Img;			// 画像表示 panel
 	JLabel label_Title;
-	JLabel label_Time;
-	JLabel label_Fin;		// 終了メッセージ
+	JTextField tfield_Min;		// 分入力欄
+	JLabel text_Colon;
+	JLabel label_Sec;
+	JLabel label_Time;			// 時間の表示場所
+	JLabel label_Fin;			// 終了メッセージ
 	JLabel label_Img;
 	ImageIcon icon;
-	int sec;				// 秒
-	int min;				// 分
+	int sec;					// 秒
+	int min;					// 分
+	int get_Min;				// 入力された分を取得する変数
 	JButton btn_Start;
 
 	// method
@@ -35,7 +40,7 @@ public class Timer_toma extends JFrame implements ActionListener{
 		// window の表示場所
 		frame_Main.setLocation( 512, 200 );
 		// window の大きさ
-		frame_Main.setSize( 256, 200 );
+		frame_Main.setSize( 400, 200 );
 		// window の大きさ固定
 		frame_Main.setResizable(false);
 		// 閉じるクリックで処理の終了
@@ -45,9 +50,17 @@ public class Timer_toma extends JFrame implements ActionListener{
 	}
 
 	public Timer_toma(){
-		label_Title = new JLabel("ラーメンTimer");
-		label_Time = new JLabel("ボタンを押して下さい");
+		// ラベルを生成
+		label_Title = new JLabel("時間を入力してください(半角数字のみ)→");
+		label_Time = new JLabel("0:00");
+		label_Time.setFont( new Font("", Font.BOLD, 30 ) );
 		label_Fin = new JLabel();
+
+		// テキストフィールド(入力欄)を生成
+		tfield_Min = new JTextField("3");
+		tfield_Min.setColumns( 2 );		// 入力欄の大きさ
+		text_Colon = new JLabel(":");
+		label_Sec = new JLabel("00");
 
 		// ボタンを生成
 		btn_Start = new JButton("Start");
@@ -55,17 +68,23 @@ public class Timer_toma extends JFrame implements ActionListener{
 		btn_Start.addActionListener(this);
 
 		// パネルを生成
-		panel_Main = new JPanel();
-		panel_Main.add( label_Title );
+			// タイトル panel
+			panel_Title = new JPanel();
+			panel_Title.add( label_Title );
+			panel_Title.add( tfield_Min );
+			panel_Title.add( text_Colon );
+			panel_Title.add( label_Sec );
 
-		panel_Label = new JPanel();
-		panel_Label.add( label_Time );
+			// 時間表示 panel
+			panel_TimeLabel = new JPanel();
+			panel_TimeLabel.add( label_Time );
 
 		// アイコンの設定
+		panel_Img = new JPanel();
 		label_Img = new JLabel();
 		icon = new ImageIcon("img/cup_noodle.png");
 		label_Img.setIcon( icon );
-		panel_Label.add( label_Img );
+		panel_Img.add( label_Img );
 
 		panel_Button = new JPanel();
 		panel_Button.add( btn_Start );
@@ -76,8 +95,9 @@ public class Timer_toma extends JFrame implements ActionListener{
 		timer.setActionCommand("timer");
 
 		// window に 各panel を載せる
-		getContentPane().add( panel_Main, BorderLayout.PAGE_START );
-		getContentPane().add( panel_Label, BorderLayout.CENTER );
+		getContentPane().add( panel_Title, BorderLayout.NORTH );
+		getContentPane().add( panel_TimeLabel, BorderLayout.CENTER );
+		getContentPane().add( panel_Img, BorderLayout.EAST );
 		getContentPane().add( panel_Button, BorderLayout.SOUTH );
 	}
 
@@ -89,14 +109,17 @@ public class Timer_toma extends JFrame implements ActionListener{
 		if( ae_Cmd.equals("start") ){
 			// 秒を指定
 			sec = 0;
+			// 入力された分を取得する
+			get_Min = Integer.parseInt( tfield_Min.getText() );
 			// 分を指定
 			min = 0;
 			// タイマーをスタートさせる
 			timer.start();
-			// ボタンを押せないようにする
+			// ボタン,入力欄を押せないようにする
 			btn_Start.setEnabled(false);
+			tfield_Min.setEnabled(false);
 			// 時間を表示
-			label_Time.setText( min + "分" + "0" + sec + "秒" );
+			label_Time.setText( min + ":" + "0" + sec );
 			// 終了メッセージを削除
 			label_Fin.setText("");
 		}
@@ -104,10 +127,10 @@ public class Timer_toma extends JFrame implements ActionListener{
 		// 時間のカウントの処理
 		if( ae_Cmd.equals("timer") ){
 			// 時間を表示
-			label_Time.setText( min + "分" + sec + "秒" );
+			label_Time.setText( min + ":" + sec );
 			// 10秒未満の時、10の位に"0"を加える
 			if( sec < 10 ){
-				label_Time.setText( min + "分" + "0" + sec + "秒" );
+				label_Time.setText( min + ":" + "0" + sec );
 			}
 			// 秒を増やす
 			sec++;
@@ -121,14 +144,15 @@ public class Timer_toma extends JFrame implements ActionListener{
 			min++;
 		}
 
-		// 3分経過したら
-		if( min >= 3 && sec == 1 ){
+		// 入力した分だけ時間が経過したら
+		if( min >= get_Min && sec == 1 ){
 			// 終了メッセージの表示
-			label_Fin.setText( min + "分経過しました");
+			label_Fin.setText( get_Min + "分経過しました");
 			// タイマーを停止
 			timer.stop();
-			// ボタンを押せるようにする
+			// ボタン,入力欄を押せるようにする
 			btn_Start.setEnabled(true);
+			tfield_Min.setEnabled(true);
 		}
 	}
 }
